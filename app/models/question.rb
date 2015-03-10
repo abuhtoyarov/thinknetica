9492fd75aac1df8a4ae01717beb1e28d0588713a
RuFalcon <falcon198603@rambler.ru>
2015-03-11 00:57:45 +0300

    Added dependent: :destroy to model question

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,6 @@
 class Question < ActiveRecord::Base
 
-	has_many :answers
+	has_many :answers, dependent: :destroy
 	
 	validates :title,:body, presence: true
 	validates :title, length: {in: 5..30}
